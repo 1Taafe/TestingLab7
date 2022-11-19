dfcac71e6908f18d0605731cb4ddf1bac8f04037
1Taafe <dima759mi@gmail.com>
2022-11-19 13:07:09 +0300

    test

--- src/main/java/org/example/TriangleDetector.java
+++ src/main/java/org/example/TriangleDetector.java
@@ -7,13 +7,7 @@ public class TriangleDetector {
     }
 
     public boolean isExists(double a, double b, double c){
-        boolean isExists = false;
-
-        if(a + b > c && a + c > b && b + c > a){
-            isExists = true;
-        }
-
-        return isExists;
+        return a + b > c && a + c > b && b + c > a;
     }
 
 }
