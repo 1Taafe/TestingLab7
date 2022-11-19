a6c620cf97d6f52040c8b315fc730fb1688b46e2
1Taafe <dima759mi@gmail.com>
2022-11-19 10:18:13 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.execute(15, 48, '*');
+
+        TriangleDetector detector = new TriangleDetector();
+        detector.isExists(10,11,12);
+
         System.out.println("Hello world!");
     }
 }
